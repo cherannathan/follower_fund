1d8273986bbd669d13f2ea31813b9dc196965f74
jeandumas <jeandumas277@gmail.com>
2021-05-25 15:11:59 +0200

    controller v2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+
+  def index
+    @users_role = User.where(role: 'creator')
+
+    if params[:genre]
+      @users = @users_role.where(genre: params[:query])
+    end
+  end
 end
