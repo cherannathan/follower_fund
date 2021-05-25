80ee3117f1f37e3a0cc272473097598602b7c005
jeandumas <jeandumas277@gmail.com>
2021-05-25 16:07:38 +0200

    pundit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,9 @@ class UsersController < ApplicationController
   def index
     @users = User.where(role: 'creator')
 
-    if params[:genre]
-      @users = @users.where(genre: params[:query])
-    end
+    # if params[:genre]
+    #   @users = @users.where(genre: params[:query])
+    # end
   end
 
   def show
