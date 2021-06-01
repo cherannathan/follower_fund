2d3851492760598135b591793f1d140f8c0dcb26
jeandumas <jeandumas277@gmail.com>
2021-06-01 17:38:20 +0200

    amelioration2

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -27,10 +27,12 @@ import "bootstrap";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
+import { toggleRange } from '../components/toggle-range';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  toggleRange();
 });
 
 import "controllers"
