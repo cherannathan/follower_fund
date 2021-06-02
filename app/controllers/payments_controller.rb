8d63695759754a71dfec0042aa5a6c306279584e
Cheran Nathan <cheran.nathan@gmail.com>
2021-06-02 13:33:51 +0200

    commit

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -2,7 +2,6 @@ class PaymentsController < ApplicationController
   def new
     @order = current_user.orders.where(state: 'pending').find(params[:order_id])
     authorize @order
-
     @order.update(state: 'done')
   end
 end
